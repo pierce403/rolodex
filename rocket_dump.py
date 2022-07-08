1a28688f9644e478fd26d55fc93b0946d5b35001
Dean Pierce <pierce403@gmail.com>
2022-07-08 05:26:03 -0700

    adding rocket dump script

---
+++ rocket_dump.py
@@ -0,0 +1,35 @@
+import requests
+import time
+import os, sys
+
+endpoint = 'https://api.rocketreach.co/v2/api/search'
+apikey = os.getenv('ROCKETKEY')
+
+# for example 'http://www.facebook.com/careers'
+website = sys.argv[1]
+
+headers = {'Api-Key':key, 'Content-Type': 'application/json'}
+
+next_page = 1
+
+while True:
+  data = f'{{"query":{{"company_website_url": ["{website}"]}}, "start": {next_page}, "page_size": 100}}'
+  print('posting request..')
+  response = requests.post(endpoint, data=data, headers=headers)
+  f = open(website+'-'+str(next_page)+".json", "w")
+  f.write(response.text)
+  f.close()
+
+  result = response.json()
+  if 'detail' in result.keys():
+    print(result['detail'])
+    if 'throttled' in result['detail']:
+      sleeptime = int(result['detail'].split(' ')[6])
+      print('sleeping for '+str(sleeptime+1)+' seconds..')
+      time.sleep(sleeptime+1)
+
+  if 'pagination' in result.keys():
+    print(result['pagination'])
+    next_page = int(result['pagination']['next'])
+    if next_page > int(result['pagination']['total']):
+      break
